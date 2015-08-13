6a51fbebb724d4aa521f4aa93fdfa3ccb33156cb
Eleonora <hildeverden@hotmail.com>
2015-08-13 15:14:23 +0200

    test

--- django-env.sh
+++
@@ -1,7 +0,0 @@
-export PATH=$PATH:/opt/local/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/site-packages/django/bin
-export PATH=$PATH:/opt/local/Library/Frameworks/Python.framework/Versions/2.7/bin
-export PATH=$PATH:/Library/Frameworks/R.framework/Versions/3.0/Resources/bin
-export DJANGO_SETTINGS_MODULE=glimpse_gui.settings
-# django smart selects 1.0.9
-# py27-django
-# py27
